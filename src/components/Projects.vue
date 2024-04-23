93221e199803ef83c33e734dfc896367401bbd3a
TimTimDev <timo.pirn@gmail.com>
2024-04-23 17:16:49 +0300

    Refactor

---
+++ src/components/Projects.vue
@@ -0,0 +1,17 @@
+<script setup>
+import ProjectCards from "../assets/cards.json"
+import ProjectsCard from './ProjectsCard.vue'
+</script>
+
+<template>
+  <div class="mt-10">
+    <h2 class="font-semibold text-green text-3xl">Client Projects</h2>
+    <p class="mt-2">Client projects I have worked on as part of a team.</p>
+  </div>
+  
+  <div class="grid grid-cols-6 gap-4 mt-8">
+
+    <ProjectsCard  />
+
+  </div>
+</template>
---
+++ src/components/ProjectsCard.vue
@@ -0,0 +1,22 @@
+<script setup>
+import ProjectCards from "../assets/cards.json"
+</script>
+
+<template>
+
+  <a v-for="card in ProjectCards.cards"
+  :href="card.link" target="_blank"
+  class="col-span-2 hover:scale-105 rounded-t" >
+    <img :src="card.imgSrc" class="rounded"/>
+    <div class="details">
+      <h3 class="text-lg font-bold text-green mt-4">
+        {{ card.title }}
+      </h3>
+      <ul class="flex justify-evenly py-4">
+        <li v-for="tech in card.techs">
+          {{ tech }}
+        </li>
+      </ul>
+    </div>
+  </a>
+</template>
