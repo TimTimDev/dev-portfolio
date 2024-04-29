734c3ea389fd82920c8c6a3fef85900d98fba2e2
TimTimDev <timo.pirn@gmail.com>
2024-04-29 14:56:02 +0300

    styling

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -4,9 +4,9 @@ import ProjectsCard from './ProjectsCard.vue'
 </script>
 
 <template>
-  <div class="mt-10">
-    <h2 class="font-semibold text-green text-3xl">Client Projects</h2>
-    <p class="mt-2">Client projects I have worked on as part of a team.</p>
+  <div class="mb-10">
+    <h2 class="font-semibold text-green text-5xl">Client Projects</h2>
+    <p class="text-3xl mt-2">Client projects I have worked on as part of a team.</p>
   </div>
   
   <div class="grid grid-cols-6 gap-6 mt-8">
