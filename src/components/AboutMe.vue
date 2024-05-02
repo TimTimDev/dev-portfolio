4c3ec35259c967a21f0b9ab688a0c26846a8235c
TimTimDev <timo.pirn@gmail.com>
2024-05-02 22:27:44 +0300

    created AboutMe component

---
+++ src/components/AboutMe.vue
@@ -0,0 +1,21 @@
+<script setup>
+import IconPerson from '../components/icons/IconPerson.vue'
+</script>
+
+<template>
+
+    <!-- <div class="rounded-full w-48 h-48 bg-gradient-radial from-green to-black-soft shadow-glow shadow-green"></div> -->
+    <div id="about" class="flex my-48">
+        <!--<IconPerson class="stroke-green scale-[5.5] mt-3.5 mr-20"/> -->
+        <div class="text-left">
+            <h2 class="text-green text-5xl mb-11 ">About Me</h2>
+            <p class="text-3xl">
+                <span class="text-green">
+                    I'm passionate about crafting delightful user experiences through clean and efficient code. With a keen eye for detail and a love for creativity, I thrive on turning ideas into reality.
+                </span> 
+
+                Whether it's building responsive websites or crafting interactive web applications, I'm dedicated to delivering high-quality solutions that leave a lasting impression.
+            </p>
+        </div>
+    </div>
+</template>
