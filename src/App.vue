ef1bd817a5cdce6330c91b1fde5fa12a4a1e8b3d
TimTimDev <timo.pirn@gmail.com>
2024-05-02 22:26:52 +0300

    removed unused components; added styling; added new components

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,27 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import AboutMe from './components/AboutMe.vue'
 import Projects from './components/Projects.vue';
 import LogoCarousel from './components/LogoCarousel.vue';
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
 </script>
 
 <template>
-  <!--
-  <header>
-    <nav>
-      <RouterLink to="/" class="text-green">Home</RouterLink>
-      <RouterLink to="/about" class="text-green">Projects</RouterLink>
-    </nav>
-    
+  
+  <header class="max-w-7xl my-0 mx-auto px-8 sticky top-0 z-50">
+    <Header />  
   </header>
-  -->
-  <div class="flex flex-col items-center text-center font-sans">
+
+  <div class="max-w-7xl my-0 mx-auto p-8 flex flex-col items-center text-center font-sans">
     <HelloWorld />
+    <AboutMe />
     <Projects />
     <LogoCarousel />
-
   </div>
 
-  <RouterView />
+  <footer>
+    <Footer />
+  </footer>
+  
 </template>
