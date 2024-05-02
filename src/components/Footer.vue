d2f987d821db1d181c8d58d92a169c5f8be132b9
TimTimDev <timo.pirn@gmail.com>
2024-05-02 22:29:12 +0300

    Created Header and Footer components

---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<script setup>
+import IconLinkedIn from '../components/icons/IconLinkedIn.vue'
+import IconGithub from '../components/icons/IconGithub.vue'
+import IconMail from '../components/icons/IconMail.vue'
+</script>
+
+<template>
+    <div id="contact" class="bg-[#0f0f0f] w-full h-[25vh] text-gray flex flex-col justify-center items-center">
+        <div class="flex justify-evenly w-2/4">
+            <a href="https://www.linkedin.com/in/timo-pirn-32b26a232/" target="_blank">
+                <IconLinkedIn class="transition duration-300 hover:text-green"/>
+            </a>
+            <a href="https://github.com/TimTimDev" target="_blank">
+                <IconGithub class="transition duration-300 hover:text-green"/>
+            </a>
+        </div>
+        
+    </div>
+</template>
